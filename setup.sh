b968c43f135b53c01686a24dc40625f108151188
feihu2024 <276652878@qq.com>
2025-10-29 16:38:37 +0800

    add school

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sqlacodegen --outfile=./model/schema.py mysql://root:123456@127.0.0.1:3306/yucheng?charset=utf8
+sqlacodegen --outfile=./model/schema.py mysql://root:123456@127.0.0.1:3306/school?charset=utf8
 sed -i 's/TAddres(/TAddress(/g' ./model/schema.py
 sed -i 's/TUserWithdrawStatu/TUserWithdrawStatus/g' ./model/schema.py
 sed -i 's/TPackageExpres/TPackageExpress/g' ./model/schema.py
