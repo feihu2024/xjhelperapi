d8edcc7ff6107f00958e0f6477eadd45fa15d224
feihu2024 <276652878@qq.com>
2025-11-02 08:31:08 +0800

    addurls

--- dao/d_admin.py
+++ dao/d_admin.py
@@ -53,50 +53,21 @@ def get_admin_by_username(username: str):
         return db.query(m_admin.TAdmin).where(m_admin.TAdmin.username == username).first()
 
 
-def query_users() -> m_admin.UserData:
-    time_now = datetime.datetime.now()
-    with Dao() as db:
-        # 统计会员总量
-        total_num = db.query(TUser).count()
+def login_shop_token(username: str, password: str, user_id:int):
+    data = {
+        'user_id': user_id,
+        'time': time.time() + SECRET.VALID_TIME
+    }
+    token = {'token_val': get_login_token_encode(data), 'user_id': user_id}
+    return token
 
-        # 根据时间条件统计日增长活跃用户数量
-        daily_add = db.query(TUser).filter(TUser.register_time >= time_now - datetime.timedelta(days=1)).count()
+def get_login_id(token:str):
+    data = get_login_token_decode(token)
+    this_time = time.time()
+    re_code = 0
+    if data:
+        if data.get('time') > this_time:
+            data['time'] = this_time + SECRET.VALID_TIME
+            re_code = int(data['user_id'])
+    return re_code
 
-        # 根据最后登录时间统计活跃数量
-        active_num = db.query(TUser).where(TUser.last_active_time >= time_now - datetime.timedelta(days=5)).count()
-
-        # 根据性别条件统计男性用户的数量
-        male = db.query(TUser).where(TUser.gender == 'male').count()
-        # 根据性别条件统计女性用户的数量
-        female = db.query(TUser).where(TUser.gender == 'female').count()
-
-        # 计算男女用户的占比
-        male_ratio: float = male / (male + female)
-        female_ratio: float = female / (male + female)
-
-        return m_admin.UserData(num=total_num, daily_add=daily_add,
-                                active=active_num, male_ratio=male_ratio, female_ratio=female_ratio)
-
-
-def silent_users(page: int = 1, page_size: int = 10):
-    """根据最后登录时间统计非活跃用户"""
-    time_now = datetime.datetime.now()
-    with Dao() as db:
-        silents = db.query(TUser). \
-            where(TUser.last_active_time < time_now - datetime.timedelta(days=10)). \
-            offset((page - 1) * page_size).limit(page_size).all()
-
-        return silents
-
-
-
-
-def query_daily_add(day: datetime.datetime.date) -> int:
-    zero_time = datetime.time()
-    zero_day = datetime.datetime.combine(day, zero_time)
-    time_condition = and_(TUser.register_time >= zero_day,
-                          TUser.register_time <= zero_day + datetime.timedelta(days=1))
-    with Dao() as db:
-        num = db.query(TUser).where(time_condition).count()
-        db.commit()
-        return num
