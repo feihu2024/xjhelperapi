4696440feab79dbf7d2ffdf81d31e634346093e6
feihu2024 <276652878@qq.com>
2025-11-05 16:54:08 +0800

    add del

--- router/admin/cl_su_kn_type.py
+++ router/admin/cl_su_kn_type.py
@@ -63,7 +63,7 @@ async def subject_update(item: SShSubject) -> str:
     d_db.update_sh_subject(item)
     return "success"
 
-@router.post(f'/subject_del', response_model=str)
+@router.get(f'/subject_del', response_model=str)
 async def subject_del(del_id: int, valcode:str ='IAqGo4QhEGET') -> str:
     d_cl_su_kn_type.del_subject(del_id)
     return "success"
